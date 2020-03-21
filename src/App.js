18477aacb6c3dc0ec106e297757c6cc3abbda7d0
fatihgozenc <fatihgozenc@gmail.com>
2020-03-21 14:03:45 +0100

    map implemented, color & breakpoint variables, fonts added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import './scss/style.scss';
 import Map from './components/Map';
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoiZmdvemVuYyIsImEiOiJjazVoMG9tMjUwY2p1M2xueHUzaXdyY2MzIn0.i6wqAEOUFdUyKEMcrUE__Q'
+});
 
 export default () => {
-  return(
-    <div>it's map</div>
+  return ( 
+    <Map style="mapbox://styles/mapbox/streets-v9"
+      containerStyle={{
+        height: '100vh',
+        width: '100vw'
+      }}
+    >
+    <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+      <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+    </Layer>
+  </Map>
   )
 }
