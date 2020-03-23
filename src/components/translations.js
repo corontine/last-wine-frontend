ebf9c6e4161ea99153667cf04a731321654c7e3f
fatihgozenc <fatihgozenc@gmail.com>
2020-03-23 03:24:52 +0100

    about title fixed

--- src/components/translations.js
+++ src/components/translations.js
@@ -9,7 +9,7 @@ const resources = {
     translation: {
       "homeTitle": "Save Berlin restaurants by buying their wine!",
       "homeButtons":["View Map", "Submit"],
-      "aboutTitle": "Info",
+      "aboutTitle": "About",
       "aboutContent": "Durch den Ausbruch der COVID-19 Krise sind viele kleine lokale Unternehmen in Schwierigkeiten gekommen und brauchen die Unterstützung der Nachbarschaft um zu überleben.<br><br>Die Öffnungszeiten von Restaurants, Bäckereien, Cafes und weiteren lokalen Unternehmen ändert sich ständig. Doch bei vielen Unternehmen kann immer noch Ware gekauft werden. Nehmt dieses Angebot an!<br><br>Melde dich bei Fragen oder Anregungen gerne an uns hallo@weinundbrot.org",
       pages: {
         home: {
@@ -20,7 +20,7 @@ const resources = {
           }
         },
         about:{
-          title: "Info",
+          title: "About",
           content: "Durch den Ausbruch der COVID-19 Krise sind viele kleine lokale Unternehmen in Schwierigkeiten gekommen und brauchen die Unterstützung der Nachbarschaft um zu überleben.<br><br>Die Öffnungszeiten von Restaurants, Bäckereien, Cafes und weiteren lokalen Unternehmen ändert sich ständig. Doch bei vielen Unternehmen kann immer noch Ware gekauft werden. Nehmt dieses Angebot an!<br><br>Melde dich bei Fragen oder Anregungen gerne an uns hallo@weinundbrot.org"
         }
       }
@@ -30,7 +30,7 @@ const resources = {
     translation: {
       "homeTitle": "Save Berlin restaurants by buying their wine!",
       "homeButtons":["View Map", "Submit"],
-      "aboutTitle": "Info",
+      "aboutTitle": "About",
       "aboutContent": "The COVID-19 outbreak is devastating bars and bakery. We thought we could help them to survive these days.<br/><br/>At the moment these small bars and bakeries could still sell their products until 6 pm.<br/><br/>Most bars and bakeries have thousands of euros in food and alcohol, which they can sell to people who are living next to them. <br/><br/> People can still enjoy their favourite bakery and wine from the bars in the corner.<br/><br/>If you have any question, please write to us hello@weinundbrot.org",
       pages: {
         home: {
